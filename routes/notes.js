cdb8a2027178afadd60d5671b7bcfd21d3769031
Furhan Samani <contact@furhan.dev>
2022-06-28 00:25:08 -0700

    add '/' route for notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,16 @@
 const notes = require('express').Router();
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const {
     readFromFile,
     readAndAppend,
     writeToFile,
 } = require('../helpers/fsUtils');
+
+notes.get('/', async (req, res) => {
+    console.log(path.join(__dirname, '../db/db.json'));
+    const data = await readFromFile(path.join(__dirname, '../db/db.json'));
+    res.json(JSON.parse(data));
+});
 
 module.exports = notes;
