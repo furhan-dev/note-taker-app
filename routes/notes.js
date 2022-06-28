e7362cd2b315e9b73dc56993464fddb94260a429
Furhan Samani <contact@furhan.dev>
2022-06-28 00:40:34 -0700

    add post route for notes

--- routes/notes.js
+++ routes/notes.js
@@ -8,9 +8,34 @@ const {
 } = require('../helpers/fsUtils');
 
 notes.get('/', async (req, res) => {
-    console.log(path.join(__dirname, '../db/db.json'));
     const data = await readFromFile(path.join(__dirname, '../db/db.json'));
     res.json(JSON.parse(data));
+});
+
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received to add note`);
+
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, path.join(__dirname, '../db/db.json'));
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in saving note');
+    }
 });
 
 module.exports = notes;
