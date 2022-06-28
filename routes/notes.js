3d06cfbddf22260e19bcdb7fe0ffbf575aa65f6f
Furhan Samani <contact@furhan.dev>
2022-06-27 21:15:21 -0700

    add routes, 404 files, initial server.js code

---
+++ routes/notes.js
@@ -0,0 +1,9 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helpers/fsUtils');
+
+module.exports = notes;
